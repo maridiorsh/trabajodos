d620732a37b509c0c8a2b2088953fee8b77a029c
maridiorsh <143105690+maridiorsh@users.noreply.github.com>
2023-10-08 21:15:06 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -19,25 +19,39 @@ import numpy as np
 import pytesseract
 from PIL import Image
 
+import streamlit as st
+import cv2
+import numpy as np
+import pytesseract
+from gtts import gTTS
+from PIL import Image
+
 st.title("Reconocimiento óptico de Caracteres")
 
-img_file_buffer = st.camera_input("Toma una Foto")
+img_file_buffer = st.file_uploader("Cargar imagen", type=["jpg", "png", "jpeg"])
 
 with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
-
+    filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
 if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    
+    # Leer la imagen
+    bytes_data = img_file_buffer.read()
+    image = Image.open(img_file_buffer)
+    st.image(image, caption="Imagen cargada", use_column_width=True)
+
+    # Convertir la imagen en texto utilizando pytesseract
+    cv2_img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
-    else:
-         cv2_img= cv2_img
+        cv2_img = cv2.bitwise_not(cv2_img)
+    text = pytesseract.image_to_string(cv2_img)
     
-        
-    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
+    # Mostrar el texto extraído
+    st.write("Texto extraído de la imagen:")
     st.write(text)
+
+    # Crear un botón para convertir el texto en audio
+    if st.button("Convertir a audio"):
+        if text:
+            tts = gTTS(text, lang='es', slow=False)  # Cambia 'es' al idioma que desees
+            audio_bytes = tts.get_audio_data()
+            st.audio(audio_bytes, format="audio/mpeg", start_time=0)
