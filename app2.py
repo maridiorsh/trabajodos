74ee62350865bebb3502136d03d7e7e6f583ce1d
maridiorsh <143105690+maridiorsh@users.noreply.github.com>
2023-10-08 20:25:42 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,43 +8,36 @@ Original file is located at
 """
 
 import streamlit as st
+import os
+import time
+import glob
+import os
+from gtts import gTTS
+from PIL import Image
 import cv2
 import numpy as np
 import pytesseract
 from PIL import Image
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
 
 st.title("Reconocimiento óptico de Caracteres")
 
 img_file_buffer = st.camera_input("Toma una Foto")
 
 with st.sidebar:
-    filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
+      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
+
 
 if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
     bytes_data = img_file_buffer.getvalue()
     cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-
+    
     if filtro == 'Con Filtro':
-        cv2_img = cv2.bitwise_not(cv2_img)
+         cv2_img=cv2.bitwise_not(cv2_img)
     else:
-        cv2_img = cv2_img
-
+         cv2_img= cv2_img
+    
+        
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text = pytesseract.image_to_string(img_rgb)
-
-    st.write("Texto Reconocido:")
+    text=pytesseract.image_to_string(img_rgb)
     st.write(text)
-
-    tts_button = Button(label="Reproducir Audio", width=100)
-
-    tts_button.js_on_event("button_click", CustomJS(code=f"""
-        var u = new SpeechSynthesisUtterance();
-        u.text = "{text}";
-        u.lang = 'es-ES';
-
-        speechSynthesis.speak(u);
-        """))
-
-    st.bokeh_chart(tts_button, use_container_width=True)
