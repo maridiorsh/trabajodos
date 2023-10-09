40050ce2025178064bb1dd896b2e0b9905acd3ed
maridiorsh <143105690+maridiorsh@users.noreply.github.com>
2023-10-08 20:44:04 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -36,8 +36,47 @@ if img_file_buffer is not None:
          cv2_img=cv2.bitwise_not(cv2_img)
     else:
          cv2_img= cv2_img
-    
         
     img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
     text=pytesseract.image_to_string(img_rgb)
     st.write(text)
+
+tld="es"
+
+def text_to_speech(text, tld):
+    
+    tts = gTTS(text,"es", tld, slow=False)
+    try:
+        my_file_name = text[0:20]
+    except:
+        my_file_name = "audio"
+    tts.save(f"temp/{my_file_name}.mp3")
+    return my_file_name, text
+
+
+#display_output_text = st.checkbox("Verifica el texto")
+
+if st.button("convertir"):
+    result, output_text = text_to_speech(text, tld)
+    audio_file = open(f"temp/{result}.mp3", "rb")
+    audio_bytes = audio_file.read()
+    st.markdown(f"## Tú audio:")
+    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+
+    #if display_output_text:
+    st.markdown(f"## Texto en audio:")
+    st.write(f" {output_text}")
+
+
+def remove_files(n):
+    mp3_files = glob.glob("temp/*mp3")
+    if len(mp3_files) != 0:
+        now = time.time()
+        n_days = n * 86400
+        for f in mp3_files:
+            if os.stat(f).st_mtime < now - n_days:
+                os.remove(f)
+                print("Deleted ", f)
+
+
+remove_files(7)
