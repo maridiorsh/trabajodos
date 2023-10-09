6f57135ef8ec8717a2845d581d143f29f47cb4b6
maridiorsh <143105690+maridiorsh@users.noreply.github.com>
2023-10-08 20:50:13 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -8,68 +8,56 @@ Original file is located at
 """
 
 import streamlit as st
-import os
-import time
-import glob
-import os
-from gtts import gTTS
-from PIL import Image
 import cv2
 import numpy as np
-import pytesseract
 from PIL import Image
+import pytesseract
+from gtts import gTTS
+import os
+import glob
+import time
 
-st.title("Reconocimiento óptico de Caracteres")
-
-img_file_buffer = st.camera_input("Toma una Foto")
-
-with st.sidebar:
-      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
-
-
-if img_file_buffer is not None:
-    # To read image file buffer with OpenCV:
-    bytes_data = img_file_buffer.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-    
-    if filtro == 'Con Filtro':
-         cv2_img=cv2.bitwise_not(cv2_img)
-    else:
-         cv2_img= cv2_img
-        
-    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
-    text=pytesseract.image_to_string(img_rgb)
-    st.write(text)
-
-tld="es"
-
+# Define la función text_to_speech
 def text_to_speech(text, tld):
-    
-    tts = gTTS(text,"es", tld, slow=False)
+    tts = gTTS(text, lang=tld, slow=False)
     try:
         my_file_name = text[0:20]
     except:
         my_file_name = "audio"
     tts.save(f"temp/{my_file_name}.mp3")
-    return my_file_name, text
+    return my_file_name
 
+# Barra lateral con opciones
+with st.sidebar:
+    filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
-#display_output_text = st.checkbox("Verifica el texto")
+# Cargar la imagen
+img_file_buffer = st.file_uploader("Cargar imagen", type=["jpg", "png", "jpeg"])
+if img_file_buffer is not None:
+    bytes_data = img_file_buffer.read()
+    image = Image.open(img_file_buffer)
+    st.image(image, caption="Imagen cargada", use_column_width=True)
 
-if st.button("convertir"):
-    result, output_text = text_to_speech(text, tld)
-    audio_file = open(f"temp/{result}.mp3", "rb")
-    audio_bytes = audio_file.read()
-    st.markdown(f"## Tú audio:")
-    st.audio(audio_bytes, format="audio/mp3", start_time=0)
+    # Convertir la imagen en texto utilizando pytesseract
+    cv2_img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    if filtro == 'Con Filtro':
+        cv2_img = cv2.bitwise_not(cv2_img)
+    text = pytesseract.image_to_string(cv2_img)
+    st.write(text)
 
-    #if display_output_text:
-    st.markdown(f"## Texto en audio:")
-    st.write(f" {output_text}")
+# Botón para convertir texto en audio
+if st.button("Convertir a audio"):
+    if text:
+        result = text_to_speech(text, tld)
+        audio_file = open(f"temp/{result}.mp3", "rb")
+        audio_bytes = audio_file.read()
+        st.audio(audio_bytes, format="audio/mp3", start_time=0)
+        st.markdown(f"## Texto en audio:")
+        st.write(f"{text}")
 
-
+# Función para eliminar archivos antiguos
 def remove_files(n):
-    mp3_files = glob.glob("temp/*mp3")
+    mp3_files = glob.glob("temp/*.mp3")
     if len(mp3_files) != 0:
         now = time.time()
         n_days = n * 86400
@@ -77,6 +65,5 @@ def remove_files(n):
             if os.stat(f).st_mtime < now - n_days:
                 os.remove(f)
                 print("Deleted ", f)
-
 
 remove_files(7)
