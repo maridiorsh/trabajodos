9e8415ba60a465271a8d2a353f67076874ce4e92
maridiorsh <143105690+maridiorsh@users.noreply.github.com>
2023-10-08 21:56:51 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -17,7 +17,6 @@ import os
 import glob
 import time
 
-# Define la función text_to_speech
 def text_to_speech(text, tld):
     tts = gTTS(text, lang=tld, slow=False)
     try:
@@ -27,28 +26,24 @@ def text_to_speech(text, tld):
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name
 
-# Barra lateral con opciones
 with st.sidebar:
     filtro = st.radio("Aplicar Filtro", ('Con Filtro', 'Sin Filtro'))
 
-# Definir tld
+#Definir tld
 tld = "es"  # Cambia "es" al idioma que desees
 
-# Cargar la imagen
 img_file_buffer = st.file_uploader("Cargar imagen", type=["jpg", "png", "jpeg"])
 if img_file_buffer is not None:
     bytes_data = img_file_buffer.read()
     image = Image.open(img_file_buffer)
     st.image(image, caption="Imagen cargada", use_column_width=True)
 
-    # Convertir la imagen en texto utilizando pytesseract
     cv2_img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
     if filtro == 'Con Filtro':
         cv2_img = cv2.bitwise_not(cv2_img)
     text = pytesseract.image_to_string(cv2_img)
     st.write(text)
 
-# Botón para convertir texto en audio
 if st.button("Convertir a audio"):
     if text:
         result = text_to_speech(text, tld)
@@ -57,8 +52,7 @@ if st.button("Convertir a audio"):
         st.audio(audio_bytes, format="audio/mp3", start_time=0)
         st.markdown(f"## Texto en audio:")
         st.write(f"{text}")
-
-# Función para eliminar archivos antiguos
+        
 def remove_files(n):
     mp3_files = glob.glob("temp/*.mp3")
     if len(mp3_files) != 0:
